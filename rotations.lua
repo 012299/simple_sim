3e7fd7af4f96856e20a049b7caa7fa86069e07fd
brontosaurus <pascalworek@gmail.com>
2017-07-18 18:27:42 +0200

    started haste calculation

--- rotations.lua
+++ rotations.lua
@@ -16,6 +16,7 @@ local ACTUAL_LENGTH_PERCENT = (FIGHT_LENGHT - 40) / FIGHT_LENGHT -- actual fight
 local BOB_CD = 90
 local EK_CD = 75
 local BASE_REGEN = 10
+local KS_COST = 40
 
 function calculate_haste_3tp(haste)
     local fp_ranks = 4 -- #TODO grab actual ranks
@@ -24,9 +25,22 @@ function calculate_haste_3tp(haste)
     local bob_cd = BOB_CD / brew_gen_sec - 6 -- There's no starvation on base energy regen for the last 6 casts in the cycle
 end
 
-function calculate_starvation(haste,energy_out,duration)
-    local haste_perc = haste/375
-    local energy_regen = BASE_REGEN * (1+haste_perc/100)
-    local energy_in = energy_regen*duration
-    
+-- Calculates after how many cycles there's not enough energy for the next KS
+-- At 0% haste, ignoring the third cycle and only calculating energy requirements for the 5th cycle, resulted in the same amount of
+-- downtime as calculating it starting at the third cycle(25 energy after calculating 3 cycles)
+function calculate_starvation(haste, energy_out, duration, bob_cd)
+    local haste_perc = haste / 375
+    local energy_regen = BASE_REGEN * (1 + haste_perc / 100)
+    local energy = 100 -- #TODO add support for racial/arcway neck
+    local energy_in = energy_regen * duration
+    local energy_diff = energy_out - energy_in
+    local seconds = 0
+    for i = 1, 10, 1 do -- redo this properly at some point
+        energy = energy - energy_diff
+        seconds = seconds + duration
+        if energy < 40 then
+            break;
+        end
     end
+
+end
