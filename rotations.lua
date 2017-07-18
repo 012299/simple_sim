67c79a07873e2527c1f42667c6ddf3d9c9d13aa7
brontosaurus <pascalworek@gmail.com>
2017-07-18 18:03:42 +0200

    updates

--- rotations.lua
+++ rotations.lua
@@ -12,12 +12,21 @@
 Energy requirement to be able to start a cycle and cast the first TP
  ]]
 local FIGHT_LENGHT = 300 -- used for BL
-local 
-function placeholder()
+local ACTUAL_LENGTH_PERCENT = (FIGHT_LENGHT - 40) / FIGHT_LENGHT -- actual fight without BL
+local BOB_CD = 90
+local EK_CD = 75
+local BASE_REGEN = 10
+
+function calculate_haste_3tp(haste)
     local fp_ranks = 4 -- #TODO grab actual ranks
-    local brew_reduction_cycle = 4.8 + 3 + (3*.1*fp_ranks) + 9 -- 3 tps, 1 KS
+    local brew_reduction_cycle = 4.8 + 3 + (3 * .1 * fp_ranks) + 9 -- 3 tps, 1 KS
     local brew_gen_sec = brew_reduction_cycle / 9
-
-
-
+    local bob_cd = BOB_CD / brew_gen_sec - 6 -- There's no starvation on base energy regen for the last 6 casts in the cycle
 end
+
+function calculate_starvation(haste,energy_out,duration)
+    local haste_perc = haste/375
+    local energy_regen = BASE_REGEN * (1+haste_perc/100)
+    local energy_in = energy_regen*duration
+    
+    end
