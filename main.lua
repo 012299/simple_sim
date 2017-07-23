7cb54b8177c4282145cadf1370e42cf62bed7cec
brontosaurus <pascalworek@gmail.com>
2017-07-23 23:29:37 +0200

    started work on ring/trinks

--- main.lua
+++ main.lua
@@ -2,6 +2,26 @@ local name, SimpleBrewSim = ...;
 local lineAdded = false
 local is_active_spec = false
 local INV_TYPES = SimpleBrewSim.INV_TYPES
+local red_loss = .76
+local green_loss = .48
+local blue_loss = .63
+local red_gain = 0
+local green_gain= 1
+local blue_gain = .51
+
+local function calculate_dps_change(new_item_link, equipped_id)
+
+    local equipped_item_link = GetInventoryItemLink("player", equipped_id)
+    -- round number workaround
+    --attempt to compare string with number
+    local dps_value = SimpleBrewSim:compare_items(equipped_item_link, new_item_link) -- string to number
+    --  print('dps value: ', dps_value)
+    local dps_str_value = SimpleBrewSim:round(SimpleBrewSim:round(math.abs(dps_value), 4), 3)
+    if dps_value < 0 then
+        return "DPS loss: " .. dps_str_value .. "%", red_loss, blue_loss, green_loss
+    end
+    return "DPS gain: " .. dps_str_value .. "%", red_gain, blue_gain, green_gain
+end
 
 local function show_dps_change()
     local _, new_item_link = GameTooltip:GetItem()
@@ -15,27 +35,17 @@ local function show_dps_change()
     if not SimpleBrewSim.ARMOUR_TYPES[item_subclass] and not SimpleBrewSim.ARMOUR_TYPES[equip_slot] then
         return
     end
+
     local equipped_id = INV_TYPES[equip_slot]
-    --- remove for now
-    --[[
-        if not equipped_id then
-            return
-        end]] --
+    local dps_string, r,g,b = calculate_dps_change(new_item_link,equipped_id)
+
     if equipped_id == INV_TYPES['INVTYPE_TRINKET'] or equipped_id == INV_TYPES['INVTYPE_FINGER'] then
-        -- get item for same link, get name
-        return
+        -- get itemlimk for same slot, get name. Do regular calculation, add line with name to tooltip
+        -- do itemlink for slot+1, get name, do regular calculation.
     end
-    local equipped_item_link = GetInventoryItemLink("player", equipped_id)
-    -- round number workaround
-    --attempt to compare string with number
-    local dps_value = SimpleBrewSim:compare_items(equipped_item_link, new_item_link) -- string to number
-    --  print('dps value: ', dps_value)
-    local dps_str_value = SimpleBrewSim:round(SimpleBrewSim:round(math.abs(dps_value), 4), 3)
-    if dps_value < 0 then
-        return "DPS loss: " .. dps_str_value .. "%", 194 / 255, 123 / 255, 160 / 255
-    end
-    return "DPS gain: " .. dps_str_value .. "%", 0, 255 / 255, 128 / 255
+    return dps_string,r,g,b
 end
+
 
 local function OnTooltipSetItem(tooltip, ...)
     if not is_active_spec then
